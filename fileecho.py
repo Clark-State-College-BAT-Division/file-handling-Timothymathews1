2a4752f5e2ce3308c6af6dc4dea753afd26a4831
Timothymathews1 <mathewstimothy2017@gmail.com>
2026-03-23 12:53:33 -0400

    Update fileecho.py

--- fileecho.py
+++ fileecho.py
@@ -1,2 +1,9 @@
 #Tead in the echo.txt file and display it in the terminal using print()
 #This is a guided practice. Either follow the video or your instructor in class.
+
+curfile = open("echo.txt")
+for curline in curfile:
+  print(curline)
+  curlfile.close()
+
+#print(curline)
