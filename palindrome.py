d18b1e30f028b12ba9d549e7e34a22c4c2dca077
Timothymathews1 <mathewstimothy2017@gmail.com>
2026-03-23 14:29:38 -0400

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -2,4 +2,11 @@
 #Read in all the words. Count how many are palindromes, or words that are spelled the same forwads and backwards.
 #For example, wow is a paldindrome.
 #A different file wile be used for grading.
-#Correct answer for this file: 
+#Correct answer for this file: 15
+count = 0
+with open("words.txt") as file:
+  for word in file:
+    word = word.strip()
+    if word == word[::-1]:
+      count +=1
+      print(count)
