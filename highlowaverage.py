faa75cdfdfad82b7320b11edc4314c17e7d3d930
Timothymathews1 <mathewstimothy2017@gmail.com>
2026-03-23 13:42:02 -0400

    Update highlowaverage.py

--- highlowaverage.py
+++ highlowaverage.py
@@ -7,3 +7,18 @@
 #Highest number
 #Lowest number
 #Correct answers for the included file: 15
+
+with opem("numbers.txt") as file:
+  numbers = []
+  for line in file:
+    numbers.append(int(line.strip()))
+    count = len(numbers)
+    total = sum(numbers)
+    average = total / count
+    highest = max(numbers)
+    lowest = min(numbers)
+    print("count:", count)
+    print("total:", total)
+    print("average:", average)
+    print("highest:", highest)
+    print("lowest:", lowest)
